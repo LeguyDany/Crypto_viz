0885a607b1bd2bdac0a99fabb0f1eab21aca4a42
Dany Leguy <danyleguy@MacBook-Pro-de-Dany.local>
2024-11-15 09:59:45 +0100

    feat(cloud): iptables + small fixes

--- ansible/roles/kafka/templates/start_server.sh
+++ ansible/roles/kafka/templates/start_server.sh
@@ -1,5 +1,26 @@
-#!/bin/sh
-{{setup_path}}/zookeeper/apache-zookeeper-3.9.3-bin/bin/zkServer.sh start 
+#!/bin/bash
+# start_servers.sh
 
-{{setup_path}}/kafka/kafka_2.13-3.9.0/bin/kafka-server-start.sh {{setup_path}}/kafka/kafka_2.13-3.9.0/config/server.properties
+# Variables
+ZOOKEEPER_PATH="/home/{{user}}/zookeeper/apache-zookeeper-3.9.3-bin"
+KAFKA_PATH="/home/{{user}}/kafka/kafka_2.13-3.9.0"
 
+# Zookeeper
+echo "Starting Zookeeper..."
+$ZOOKEEPER_PATH/bin/zkServer.sh start
+
+# Wait for Zookeeper to start
+sleep 10
+
+# Check Zookeeper status
+echo "Checking Zookeper's status..."
+$ZOOKEEPER_PATH/bin/zkServer.sh status
+
+# Start Kafka
+echo "Starting Kafka..."
+$KAFKA_PATH/bin/kafka-server-start.sh -daemon $KAFKA_PATH/config/server.properties
+
+# Check Kafka status
+echo "Checking kafka's status..."
+sleep 10
+$KAFKA_PATH/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
