5e75d4f27a9df44fd9efb2dca8f71dc22b1cf04e
Dany Leguy <danyleguy@MacBook-Pro-de-Dany.local>
2024-11-15 15:43:20 +0100

    feat(cloud): Better ansible zookeeper myid automation + terraform security group

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -4,6 +4,14 @@ resource "aws_security_group" "zookeeper_kafka_sg" {
   description = "Allow SSH inbound traffic"
   vpc_id      = aws_vpc.main.id  
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # === SSH ===
   ingress {
     from_port   = 22
     to_port     = 22
@@ -11,11 +19,27 @@ resource "aws_security_group" "zookeeper_kafka_sg" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  # === Zookeeper ===
+  # Client port
+  ingress { 
+    from_port   = 2181
+    to_port     = 2181
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  # Leader election port
+  ingress {
+    from_port   = 3888
+    to_port     = 3888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  # Quorum port
+  ingress {
+    from_port   = 2888
+    to_port     = 2888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   tags = {
