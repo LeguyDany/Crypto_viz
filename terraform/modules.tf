a82894e36347a5296cd328855a15b6c063e0b84f
Dany Leguy <danyleguy@MacBook-Pro-de-Dany.local>
2024-12-19 15:38:07 +0100

    feat(cloud/spark): spark setup

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -1,11 +1,17 @@
-module "kafka" {
-  source            = "./kafka"
-  vpc_id            = aws_vpc.main.id
-  instance_count    = var.instance_count_kafka
-  route_table_id    = aws_route_table.main.id
-  aws_ami_debian_id = data.aws_ami.debian.id
-  aws_key_pair_name = aws_key_pair.deployer.key_name
-}
+# module "kafka" {
+#   source            = "./kafka"
+#   vpc_id            = aws_vpc.main.id
+#   instance_count    = var.instance_count_kafka
+#   route_table_id    = aws_route_table.main.id
+#   aws_ami_debian_id = data.aws_ami.debian.id
+#   aws_key_pair_name = aws_key_pair.deployer.key_name
+# }
+# output "kafka_elastic_public_ips" {
+#   value = module.kafka.public_elastic_ips_kafka
+# }
+# output "kafka_elastic_private_ips" {
+#   value = module.kafka.private_elastic_ips_kafka
+# }
 
 module "spark_consumer" {
   source         = "./spark_consumer"
@@ -26,15 +32,7 @@ module "spark_producer" {
 }
 
 
-
 # Outputs
-
-output "kafka_elastic_public_ips" {
-  value = module.kafka.public_elastic_ips_kafka
-}
-output "kafka_elastic_private_ips" {
-  value = module.kafka.private_elastic_ips_kafka
-}
 
 output "spark_consumer_public_ips" {
   value = module.spark_consumer.public_ip_spark_consumer
--- terraform/spark_consumer/security_group.tf
+++ terraform/spark_consumer/security_group.tf
@@ -28,6 +28,37 @@ resource "aws_security_group" "spark_consumer_sg" {
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
+  # === Spark ===
+  # Master port
+  ingress { 
+    from_port   = 7077 
+    to_port     = 7077
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  # Worker port
+  ingress {
+    from_port   = 7080
+    to_port     = 7080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+  # Web UI master port
+  ingress {
+    from_port   = 7080
+    to_port     = 7080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Web UI worker port
+  ingress {
+    from_port   = 7081
+    to_port     = 7081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   tags = {
     Name = "spark-consumer-sg"
   }
